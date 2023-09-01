60152cdac353a7892d3ae80d5a2d6d6e5f94d2df
pqr <pqr@gmail.com>
2023-09-01 13:56:10 +0530

    v4

--- A.java
+++ A.java
@@ -1,11 +1,9 @@
-class Add{
-
-     public static void main(String args[])
+   class A{
+   public static void main(String args[])
      {
      
        System.out.println("Hello World");
        System.out.println("Welcome");
          
      }
-
-}
+     }
